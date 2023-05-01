4b8eb45f6784f907ae271aed55f8a6c797a226bb
ermaozi <admin@ermao.net>
2023-05-01 00:18:11 +0000

    涨价

--- src/components/common/shop/commodity/Recharge.vue
+++ src/components/common/shop/commodity/Recharge.vue
@@ -2,7 +2,7 @@
     <n-card>
         <h1>自建机器人充值</h1>
         <p>只有自建机器人可以买这个，可以将购买到的天数分配给自建机器人加的群，使其变成付费群</p>
-        <p>价格：5 元/月</p>
+        <p>价格：8 元/月</p>
         <n-input-number min=1 max=12 :show-button="false" :bordered="false" placeholder="1 到 12">
         <template #prefix>
             充
