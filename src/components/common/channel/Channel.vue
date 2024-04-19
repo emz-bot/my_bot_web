13989f4cde860cee2c8269fb9592a7bdb15cb857
ermaozi <admin@ermao.net>
2024-04-19 10:36:56 +0000

    添加搜索  添加关闭聊天室

--- src/components/common/channel/Channel.vue
+++ src/components/common/channel/Channel.vue
@@ -1,41 +1,46 @@
 <template>
   <n-space>
-    <n-button @click="">搜索</n-button>
+    <n-input type="text" v-model:value="searchTerm" @keyup.enter="search" placeholder="搜索..." style="width: 50px;" />
     <n-button type="success" quaternary circle @click="showModal = true" style="margin-bottom: 10px;">
       <n-icon size="22">
         <Add />
       </n-icon>
     </n-button>
     <n-modal v-model:show="showModal">
-    <n-card
-      style="width: 600px"
-      title="创建频道"
-      :bordered="false"
-      size="huge"
-      role="dialog"
-      aria-modal="true"
-    >
+      <div style="position: relative;">
 
-      <n-input v-model:value="channelName" placeholder="请输入频道名称"></n-input>
-      <template #footer>
-        <div style="display: flex; justify-content: flex-end;">
-        <n-space>
-          <n-button  @click="api_create_channel">确定</n-button>
-          <n-button  @click="handleCancel">取消</n-button>
-        </n-space>
+        <n-card style="width: 600px" title="创建频道" :bordered="false" size="huge" role="dialog" aria-modal="true">
+
+          <n-input v-model:value="channelName" placeholder="请输入频道名称"></n-input>
+          <template #footer>
+            <div style="display: flex; justify-content: flex-end;">
+              <n-space>
+                <n-button @click="api_create_channel">确定</n-button>
+                <n-button @click="handleCancel">取消</n-button>
+              </n-space>
+            </div>
+          </template>
+        </n-card>
+        <n-spin v-if="isLoading"
+          style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"></n-spin>
       </div>
-      <loading-spinner v-if="isLoading"></loading-spinner>
-      </template>
-    </n-card>
-  </n-modal>
+    </n-modal>
   </n-space>
   <n-space style="display: flex;">
     <n-scrollbar style="max-height: 600px">
       <div style="width: 100px;">
         <n-space vertical>
-          <n-button v-for="(panel, index) in panelsRef" :key="index" @click="handleClick(panel)"
-          :class="{ 'active-button': selectedPanel === panel }"
-          style="width: 100px; text-overflow: ellipsis; white-space: nowrap;">{{ panel }}</n-button>
+          <div v-for="(panel, index) in panelsRef" :key="index" class="panel-button"
+            style="display: flex; align-items: center; position: relative;">
+            <n-button @click="handleClick(panel)" :class="{ 'active-button': selectedPanel === panel }"
+              style="width: 100%; text-overflow: ellipsis; white-space: nowrap;">{{ panel }}</n-button>
+            <n-button class="close-button" @click="handleClose(panel)" text
+              style="padding: 5px; visibility: hidden; position: absolute; right: 0; top: 50%; transform: translateY(-50%);">
+              <n-icon>
+                <CloseOutline />
+              </n-icon>
+            </n-button>
+          </div>
         </n-space>
       </div>
     </n-scrollbar>
@@ -49,44 +54,61 @@
 .active-button {
   background-color: rgb(51, 56, 53) !important;
 }
+
+.panel-button:hover .close-button {
+  visibility: visible !important;
+}
 </style>
 
 <script setup>
 import { Add } from '@vicons/ionicons5'
 import { ref, inject, watch, onMounted } from 'vue';
 import Chat from "./Chat.vue";
-import { useMessage, NButton, NSpace, NScrollbar,NIcon,NCard,NModal,NInput,LoadingSpinner} from "naive-ui";
+import { useMessage, NButton, NSpace, NScrollbar, NIcon, NCard, NModal, NInput, NSpin, NTooltip } from "naive-ui";
+import { CloseOutline } from '@vicons/ionicons5'
 import { create_channel } from '@/utils/jianghu_api';
 
 
 
 const messages = inject('channel_message');
-const showModal= ref(false)
+const showModal = ref(false)
 const channelName = ref('')
 const nameRef = ref(1)
 const message = useMessage()
 const selectedPanel = ref(null)
 const panelsRef = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+const isLoading = ref(false)
+const searchTerm = ref('');
 
-
-let isLoading = ref(false)
-
-async function api_create_channel(){
-    isLoading.value = true
-    await create_channel({"channel_name": channelName.value}).then((res) => {
+async function api_create_channel() {
+  isLoading.value = true
+  try {
+    const res = await create_channel({ "channel_name": channelName.value });
     if (res.code == 200) {
       message.success('创建成功')
       showModal.value = false
     } else {
       console.log(res.msg)
     }
-    isLoading.value = false
-  }).catch((error) => {
+  } catch (error) {
     console.error(error)
+    message.error('请求失败: ' + error.message)
+  } finally {
     isLoading.value = false
-  });
+  }
 }
 
+
+function search() {
+  const searchValue = Number(searchTerm.value);
+  const panel = panelsRef.value.find(p => p === searchValue);
+  console.log( searchValue,panelsRef.value, panel);
+  if (panel) {
+    handleClick(panel);
+  } else {
+    console.log('未找到匹配的聊天室');
+  }
+}
 function handleCancel() {
   showModal.value = false
 }
@@ -129,7 +151,7 @@ function handleClose(name) {
     message.error('最后一个了')
     return
   }
-  message.info('关掉 ' + name)
+  // message.info('关掉 ' + name)
   const index = panels.findIndex((v) => name === v)
   panels.splice(index, 1)
   if (nameRef.value === name) {
