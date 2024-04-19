cef1c5d2fc1ddb557871f967e9e2617d71fae2bc
ermaozi <admin@ermao.net>
2024-04-19 01:36:43 +0000

    修改聊天室列表靠左，限制高度，

--- src/components/common/channel/Channel.vue
+++ src/components/common/channel/Channel.vue
@@ -1,41 +1,57 @@
 <template>
-     <div>
-    <n-tabs
-    v-model:value="nameRef"
-    type="card"
-    closable
-    tab-style="min-width: 80px;"
-    @keydown="handleKeyDown"
-    @close="handleClose"
-  >
-    <n-tab-pane
-      v-for="panel in panelsRef"
-      :key="panel"
-      :tab="panel.toString()"
-      :name="panel"
-    >
-      <Chat :chatRoomId="panel" />
-    </n-tab-pane>
-  </n-tabs>
-  </div>
+  <n-space style="display: flex;">
+    <n-scrollbar style="max-height: 600px">
+      <div style="width: 100px;">
+        <n-space vertical>
+          <n-button v-for="(panel, index) in panelsRef" :key="index" @click="handleClick(panel)"
+            :class="{ 'active-button': selectedPanel === panel }"
+            style="width: 100px; text-overflow: ellipsis; white-space: nowrap;">{{ panel }}</n-button>
+        </n-space>
+      </div>
+    </n-scrollbar>
+    <div style="width: 400px;">
+      <Chat :chatRoomId="selectedPanel" />
+    </div>
+  </n-space>
 </template>
 
+<style scoped>
+.active-button {
+  background-color: rgb(51, 56, 53) !important;
+}
+</style>
+
 <script setup>
-import {ref} from 'vue';
+import { ref, inject, watch, onMounted } from 'vue';
 import Chat from "./Chat.vue";
-import {useMessage,NTabs,NTabPane} from "naive-ui";
+import { useMessage, NButton, NSpace, NScrollbar } from "naive-ui";
+
+
+
+const messages = inject('channel_message');
 
 const nameRef = ref(1)
 const message = useMessage()
+const selectedPanel = ref(null)
 const panelsRef = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 
+
+
+
+function handleClick(panel) {
+  selectedPanel.value = panel
+}
+
 function handleKeyDown(event) {
-  console.log(event.key)
   if (event.key === 'Tab') {
     event.preventDefault(); // 阻止默认的Tab键行为
     switchTab(event.shiftKey);
   }
 }
+
+onMounted(() => {
+  selectedPanel.value = panelsRef.value[0]
+})
 
 function switchTab(isShiftKey) {
   const { value: panels } = panelsRef;
@@ -46,17 +62,21 @@ function switchTab(isShiftKey) {
   nameRef.value = panels[nextIndex];
 }
 
-function handleClose (name) {
-    const { value: panels } = panelsRef
-    if (panels.length === 1) {
+watch(messages.value, () => {
+  console.log('messages.value', messages.value[selectedPanel.value])
+});
+
+function handleClose(name) {
+  const { value: panels } = panelsRef
+  if (panels.length === 1) {
     message.error('最后一个了')
     return
-    }
-    message.info('关掉 ' + name)
-    const index = panels.findIndex((v) => name === v)
-    panels.splice(index, 1)
-    if (nameRef.value === name) {
+  }
+  message.info('关掉 ' + name)
+  const index = panels.findIndex((v) => name === v)
+  panels.splice(index, 1)
+  if (nameRef.value === name) {
     nameRef.value = panels[index]
-    }
+  }
 }
 </script>
