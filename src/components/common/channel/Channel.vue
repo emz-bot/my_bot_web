1345192f49bd73248cc9889931bd7ad69768a5f1
ermaozi <admin@ermao.net>
2024-04-23 02:06:01 +0000

    修改创建和加入频道方法

--- src/components/common/channel/Channel.vue
+++ src/components/common/channel/Channel.vue
@@ -7,31 +7,35 @@
       </n-icon>
     </n-button>
     <n-modal v-model:show="showModal">
-    <div style="position: relative;">
-      <n-card style="width: 600px" title="创建或加入频道" :bordered="false" size="huge" role="dialog" aria-modal="true">
-        <n-select v-model:value="channelAction" :options="channelActions" placeholder="请选择操作"></n-select>
-        <n-input v-model:value="channelName" :placeholder="placeholderText"></n-input>
-        <template #footer>
-          <div style="display: flex; justify-content: flex-end;">
-            <n-space>
-              <n-button @click="handleChannelAction">确定</n-button>
-              <n-button @click="handleCancel">取消</n-button>
-            </n-space>
-          </div>
-        </template>
-      </n-card>
+      <div style="position: relative;">
+        <n-card style="width: 600px" title="创建或加入频道" :bordered="false" size="huge" role="dialog" aria-modal="true">
+          <n-radio-group v-model:value="channelAction">
+            <n-radio :label="'create'">创建频道</n-radio>
+            <n-radio :label="'join'">加入频道</n-radio>
+          </n-radio-group>
+          <n-input v-model:value="channelName" :placeholder="placeholderText"></n-input>
+          <template #footer>
+            <div style="display: flex; justify-content: flex-end;">
+              <n-space>
+                <n-button @click="handleChannelAction">确定</n-button>
+                <n-button @click="handleCancel">取消</n-button>
+              </n-space>
+            </div>
+          </template>
+        </n-card>
         <n-spin v-if="isLoading"
           style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"></n-spin>
       </div>
     </n-modal>
   </n-space>
-  <n-space style="display: flex;">
+  <n-space style="di1splay: flex;">
     <n-scrollbar style="max-height: 600px">
       <div style="width: 100px;">
         <n-space vertical>
           <div v-for="(panel, index) in panelsRef" :key="index" class="panel-button"
             style="display: flex; align-items: center; position: relative;">
-            <n-button @click="handleClick(panel.channel_id)" :class="{ 'active-button': selectedPanel === panel.channel_id }"
+            <n-button @click="handleClick(panel.channel_id)"
+              :class="{ 'active-button': selectedPanel === panel.channel_id }"
               style="width: 100%; text-overflow: ellipsis; white-space: nowrap;">{{ panel.channel_name }}</n-button>
             <n-button class="close-button" @click="handleClose(panel.channel_id)" text
               style="padding: 5px; visibility: hidden; position: absolute; right: 0; top: 50%; transform: translateY(-50%);">
@@ -61,13 +65,15 @@
 
 <script setup>
 import { Add } from '@vicons/ionicons5'
-import { ref, inject, watch, onMounted,computed} from 'vue';
+import { ref, inject, watch, onMounted, computed } from 'vue';
 import Chat from "./Chat.vue";
-import { useMessage, NButton, NSpace, NScrollbar, NIcon, NCard, NModal, NInput, NSpin, NTooltip,NSelect } from "naive-ui";
+import { useMessage, NButton, NSpace, NScrollbar, NIcon, NCard, NModal, NInput, NSpin, NRadio, NRadioGroup } from "naive-ui";
 import { CloseOutline } from '@vicons/ionicons5'
-import { create_channel, get_channel_list,join_channel } from '@/utils/jianghu_api';
+import { create_channel, get_channel_list, join_channel } from '@/utils/jianghu_api';
 
 
+
+const channelAction = ref('');
 
 const messages = inject('channel_message');
 const showModal = ref(false)
@@ -79,14 +85,10 @@ const panelsRef = ref([])
 const isLoading = ref(false)
 const searchTerm = ref('');
 
-const channelAction = ref(null)
-const channelActions = [
-  { label: '创建频道', value: 'create' },
-  { label: '加入频道', value: 'join' }
-]
 
 async function handleChannelAction() {
   if (channelAction.value === 'create') {
+    console.log(channelAction.value)
     await api_create_channel()
   } else if (channelAction.value === 'join') {
     await api_join_channel()
@@ -167,15 +169,15 @@ function handleKeyDown(event) {
 }
 
 const fetchChannelList = async () => {
-      const res = await get_channel_list()
-      if (res.code === 200) {
-        // console.log(res.data)
-        panelsRef.value = res.data
-        return res.data
-      } else {
-        message.error(res.msg)
-      }
-    }
+  const res = await get_channel_list()
+  if (res.code === 200) {
+    // console.log(res.data)
+    panelsRef.value = res.data
+    return res.data
+  } else {
+    message.error(res.msg)
+  }
+}
 
 onMounted(() => {
   var a = fetchChannelList()
