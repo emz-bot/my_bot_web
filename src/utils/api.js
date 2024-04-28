6a8f269c0506da97a901221984ad5e2e2a8dc140
ermaozi <admin@ermao.net>
2024-04-28 16:32:29 +0800

    同步前端路由

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,6 @@ import { get, post } from './http'
 
 // BotManage
 
-export const login = data => post('/api/login', data)
-export const register = data => post('/api/register', data)
-export const api_verification_code = data => get('/api/register', data)
+export const login = data => post('/api/v1/user/login', data)
+export const register = data => post('/api/v1/user/register', data)
+export const api_verification_code = data => get('/api/v1/user/register', data)
--- src/utils/jianghu_api.js
+++ src/utils/jianghu_api.js
@@ -1,16 +1,14 @@
 import { get, post, uploadFile } from './http'
 
-export const upload_avatar = data => uploadFile('/jianghu/v1/user/upload_avatar', data)
-export const set_user_info = data => post('/jianghu/v1/user/set_user_info', data)
+export const upload_avatar = data => uploadFile('/api/v1/user/upload_avatar', data)
+export const set_user_info = data => post('/api/v1/user/set_user_info', data)
 
-export const create_channel = data => post('/jianghu/v1/channel/create_channel', data)
-export const get_channel_list = data => get('/jianghu/v1/channel/channel_list', {})
-export const join_channel = data => post('/jianghu/v1/channel/join_channel', data)
-export const leave_channel = data => post('/jianghu/v1/channel/leave_channel', data)
-export const delete_channel = data => post('/jianghu/v1/channel/delete_channel', data)
-
-export const get_sys_msg = data => get('/jianghu/v1/channel/sys_msg', {})
-export const read_sys_msg = data => post('/jianghu/v1/channel/read_sys_msg', data)
-
-export const accept_channel_apply = data => post('/jianghu/v1/channel/accept_apply', data)
-export const reject_channel_apply = data => post('/jianghu/v1/channel/reject_apply', data)
+export const create_channel = data => post('/api/v1/channel/create_channel', data)
+export const get_channel_list = data => get('/api/v1/channel/channel_list', {})
+export const join_channel = data => post('/api/v1/channel/join_channel', data)
+export const leave_channel = data => post('/api/v1/channel/leave_channel', data)
+export const delete_channel = data => post('/api/v1/channel/delete_channel', data)
+export const get_sys_msg = data => get('/api/v1/channel/sys_msg', {})
+export const read_sys_msg = data => post('/api/v1/channel/read_sys_msg', data)
+export const accept_channel_apply = data => post('/api/v1/channel/accept_apply', data)
+export const reject_channel_apply = data => post('/api/v1/channel/reject_apply', data)
